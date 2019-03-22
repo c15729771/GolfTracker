314e7c79d42edaba6d76ac8dffab60241edc5816
Killian <c15729771@mydit.ie>
2019-03-22 20:17:40 +0000

    test for python anywhere

--- Golfjournal/settings.py
+++ Golfjournal/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'pwa',
+   # 'pwa',
 ]
 #
 # INSTALLED_APPS = {
@@ -145,6 +145,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT ='/home/c15729771/GolfTracker/static'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
