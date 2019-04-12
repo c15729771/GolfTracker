921077580601ee0c5b68c05736f791765b3a4a19
c15729771 <c15729771@mydit.ie>
2019-04-12 11:14:29 +0000

    main css file updated to make project look better etc

--- static/Golf_Journal/js/script.js
+++ static/Golf_Journal/js/script.js
@@ -1,105 +1,105 @@
-$(document).ready(function(){
-    if($('#result') != null){
-       // Read();
-    }
-    $('#create').on('click', function(){
-        $firstname = $('#firstname').val();
-        $lastname = $('#lastname').val();
+// $(document).ready(function(){
+//     if($('#result') != null){
+//       // Read();
+//     }
+//     $('#create').on('click', function(){
+//         $firstname = $('#firstname').val();
+//         $lastname = $('#lastname').val();
 
-        if($firstname == "" || $lastname == ""){
-            alert("Please complete the required field");
-        }else{
-            $.ajax({
-                url: 'create',
-                type: 'POST',
-                data: {
-                    firstname: $firstname,
-                    lastname: $lastname,
-                    csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
-                },
-                success: function(){
-                    Read();
-                    populateGolfGamesTable();
-                    $('#firstname').val('');
-                    $('#lastname').val('');
-                }
-            });
-        }
-    });
+//         if($firstname == "" || $lastname == ""){
+//             alert("Please complete the required field");
+//         }else{
+//             $.ajax({
+//                 url: 'create',
+//                 type: 'POST',
+//                 data: {
+//                     firstname: $firstname,
+//                     lastname: $lastname,
+//                     csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+//                 },
+//                 success: function(){
+//                     Read();
+//                     populateGolfGamesTable();
+//                     $('#firstname').val('');
+//                     $('#lastname').val('');
+//                 }
+//             });
+//         }
+//     });
 
-    $(document).on('click', '.edit', function(){
-        $id = $(this).attr('name');
-        window.location = "edit/" + $id;
-    });
+//     $(document).on('click', '.edit', function(){
+//         $id = $(this).attr('name');
+//         window.location = "edit/" + $id;
+//     });
 
-    $('#update').on('click', function(){
-        $firstname = $('#firstname').val();
-        $lastname = $('#lastname').val();
+//     $('#update').on('click', function(){
+//         $firstname = $('#firstname').val();
+//         $lastname = $('#lastname').val();
 
-        if($firstname == "" || $lastname == ""){
-            alert("Please complete the required field");
-        }else{
-            $id = $('#member_id').val();
-            $.ajax({
-                url: 'update/' + $id,
-                type: 'POST',
-                data: {
-                    firstname: $firstname,
-                    lastname: $lastname,
-                    csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
-                },
-                success: function(){
-                    window.location = '/';
-                    alert('Updated!');
-                }
-            });
-        }
+//         if($firstname == "" || $lastname == ""){
+//             alert("Please complete the required field");
+//         }else{
+//             $id = $('#member_id').val();
+//             $.ajax({
+//                 url: 'update/' + $id,
+//                 type: 'POST',
+//                 data: {
+//                     firstname: $firstname,
+//                     lastname: $lastname,
+//                     csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+//                 },
+//                 success: function(){
+//                     window.location = '/';
+//                     alert('Updated!');
+//                 }
+//             });
+//         }
 
-    });
-/*
-    $(document).on('click', '.delete', function(){
-        $id = $(this).attr('name');
-        $.ajax({
-            url: 'delete/' + $id,
-            type: 'POST',
-            data: {
-                csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
-            },
-            success: function(){
-                Read();
-                alert("Deleted!");
-            }
-        });
-    });*/
+//     });
+// /*
+//     $(document).on('click', '.delete', function(){
+//         $id = $(this).attr('name');
+//         $.ajax({
+//             url: 'delete/' + $id,
+//             type: 'POST',
+//             data: {
+//                 csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+//             },
+//             success: function(){
+//                 Read();
+//                 alert("Deleted!");
+//             }
+//         });
+//     });*/
 
-});
+// });
 
-function Read(){
-    $.ajax({
-		url: 'read',
-		type: 'POST',
-		async: true,
-		data:{
-			res: 1,
-			csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
-		},
-		success: function(response){
-			$('#result').html(response);
-		}
-    });
-}
+// function Read(){
+//     $.ajax({
+// 		url: 'read',
+// 		type: 'POST',
+// 		async: true,
+// 		data:{
+// 			res: 1,
+// 			csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+// 		},
+// 		success: function(response){
+// 			$('#result').html(response);
+// 		}
+//     });
+// }
 
-function populateGolfGamesTable(){
-    $.ajax({
-		url: 'getGameEntries',
-		type: 'POST',
-		async: true,
-		data:{
-			res: 1,
-			csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
-		},
-		success: function(response){
-			$('#golfCourseTable').html(response);
-		}
-    });
-}
+// function populateGolfGamesTable(){
+//     $.ajax({
+// 		url: 'getGameEntries',
+// 		type: 'POST',
+// 		async: true,
+// 		data:{
+// 			res: 1,
+// 			csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+// 		},
+// 		success: function(response){
+// 			$('#golfCourseTable').html(response);
+// 		}
+//     });
+// }
